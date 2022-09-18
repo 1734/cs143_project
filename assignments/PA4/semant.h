1b3444b24743d1f68a832e5e74306416e1fad7c9
1734 <30361733+1734@users.noreply.github.com>
2022-09-18 23:14:58 +0800

    Update semant.h

--- assignments/PA4/semant.h
+++ assignments/PA4/semant.h
@@ -38,7 +38,6 @@ public:
   ostream& semant_error(Class_ c);
   ostream& semant_error(Symbol filename, tree_node *t);
 
-  std::map<Symbol, Class_> map_symbol_to_class;
   std::map<Symbol, Symbol> map_to_parent;
   std::map<Symbol, std::vector<Symbol>> map_to_children;
   enum color { WHITE, GREY, BLACK };
@@ -50,8 +49,11 @@ public:
 
   void dfs_inheritance_tree_for_feature_type(Symbol);
 
+  void check_main();
+
 };
 
+extern std::map<Symbol, Class_> map_symbol_to_class;
 extern SymbolTable<Symbol, Symbol> object_name_to_type_table;
 extern std::map<Symbol, std::map<Symbol, std::pair<Symbol, Formals>>> map_class_to_map_method_to_types;
 extern std::map<Symbol, std::map<Symbol, Symbol>> map_class_to_map_attr_to_type;
